98fe9abeb92c8ddf4d385663f81d91530d082b03
GiorgioDellutri <giorgiodll93@gmail.com>
2023-01-27 18:47:22 +0100

    created for

---
+++ script.js
@@ -0,0 +1,22 @@
+const { createApp } = Vue;
+createApp({
+    data() {
+        return {
+            apiURL: "./server.php",
+            diskList: [],
+        }
+    },
+    methods: {
+        getAPI() {
+            axios.get(this.apiURL)
+              .then((response) => {
+                console.log(response.data);
+                this.diskList = response.data;
+                 console.log(this.diskList);
+            });
+        },
+    },
+    created() {
+        this.getAPI();
+    },
+}).mount('#app');
---
+++ server.php
@@ -0,0 +1,16 @@
+<?php 
+$getJson = file_get_contents(__DIR__ . '/db/dischi.json');
+$array = json_decode($getJson,true);
+
+// if ($array != null) {
+//  }
+//  var_dump($array);
+
+
+if ($array != null) {
+    header('Content-Type: application/json');
+
+    echo json_encode($array);
+}
+
+?>
